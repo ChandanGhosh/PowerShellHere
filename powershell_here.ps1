99c24bd0492434c22fc1f8dd8969935b129e48ba
Chandan Ghosh <ckghosh1983@gmail.com>
2018-04-25 23:28:18 +0530

    files updated

--- powershell_here.ps1
+++ powershell_here.ps1
@@ -1,24 +1,32 @@
 ﻿# powershell here.
 # powershell path
-$command = "$PSHOME\powershell.exe -NoExit -NoProfile -Command ""Set-Location '%V'"""
+$pspath= "$PSHOME\powershell.exe -NoExit"
 
-# for right clicking on the background in drives, folders
-$menu1 = 'Open PowerShell Here'
+# for right clicking on folders
+New-Item HKLM:\SOFTWARE\Classes\Directory\shell\PSOpenHere -force 
+Set-Item HKLM:\SOFTWARE\Classes\Directory\shell\PSOpenHere "Open PowerShell Here"
+New-item HKLM:\SOFTWARE\Classes\Directory\shell\PSOpenHere\command -force
+Set-item HKLM:\SOFTWARE\Classes\Directory\shell\PSOpenHere\command "$pspath Set-Location '%L'"
 
-'directory', 'directory\background', 'drive' | ForEach-Object {
-    New-Item -Path "Registry::HKEY_CLASSES_ROOT\$_\shell" -Name runas\command -Force |
-    Set-ItemProperty -Name '(default)' -Value $command -PassThru |
-    Set-ItemProperty -Path {$_.PSParentPath} -Name '(default)' -Value $menu1 -PassThru 
-    #|Set-ItemProperty -Name HasLUAShield -Value ''
-}
+# for right clicking on drives
+New-Item HKLM:\SOFTWARE\Classes\Drive\shell\PSOpenHere -force 
+Set-Item HKLM:\SOFTWARE\Classes\Drive\shell\PSOpenHere "Open PowerShell Here"
+New-item HKLM:\SOFTWARE\Classes\Drive\shell\PSOpenHere\command -force
+Set-item HKLM:\SOFTWARE\Classes\Drive\shell\PSOpenHere\command "$pspath Set-Location '%L'"
+
+# for right clicking on the background of folders
+New-Item HKLM:\SOFTWARE\Classes\Directory\Background\shell\PSOpenHere -force 
+Set-Item HKLM:\SOFTWARE\Classes\Directory\Background\shell\PSOpenHere "Open PowerShell Here"
+New-item HKLM:\SOFTWARE\Classes\Directory\Background\shell\PSOpenHere\command -force
+Set-item HKLM:\SOFTWARE\Classes\Directory\Background\shell\PSOpenHere\command "$pspath"
 
 # for right clicking on the background as an administrator in drives, folders
-$menu2 = 'Open PowerShell(As Administrator) Here'
+$menu = 'Open PowerShell(As Administrator) Here'
 $command = "$PSHOME\powershell.exe -NoExit -NoProfile -Command ""Set-Location '%V'"""
  
 'directory', 'directory\background', 'drive' | ForEach-Object {
     New-Item -Path "Registry::HKEY_CLASSES_ROOT\$_\shell" -Name runas\command -Force |
     Set-ItemProperty -Name '(default)' -Value $command -PassThru |
-    Set-ItemProperty -Path {$_.PSParentPath} -Name '(default)' -Value $menu2 -PassThru |
+    Set-ItemProperty -Path {$_.PSParentPath} -Name '(default)' -Value $menu -PassThru |
     Set-ItemProperty -Name HasLUAShield -Value ''
 }
